0b13d696c1f779bb00f75dacb56e5fdc95c0317b
Jaskaranbir <jaskaranbir.dhillon@gmail.com>
2018-09-10 02:23:01 -0400

    Add model: EventStoreQuery

---
+++ model/eventstore_query.go
@@ -0,0 +1,11 @@
+package model
+
+// EventStoreQuery can be used to fetch later events than the specified version.
+type EventStoreQuery struct {
+	// AggregateID is the id for aggregate whose events are to be fetched
+	AggregateID string `json:"aggregate_id"`
+	// AggregateVersion is the highest version of events that have been
+	// already fetched by the aggregate. The event-store will be queried
+	// for events greater than this version.
+	AggregateVersion int64 `json:"aggregate_version"`
+}
