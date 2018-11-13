c345dd0a6cfb254f820ae97d7feb3ebd88d1b0bb
Jaskaranbir <jaskaranbir.dhillon@gmail.com>
2018-11-12 20:58:36 -0500

    Rename KafkaResponse to Document

--- model/document.go
+++ model/document.go
@@ -2,14 +2,12 @@ package model
 
 import "github.com/TerrexTech/uuuid"
 
-// KafkaResponse is the response from consuming a Kafka message
-// and operating on it. This can be used to as a "response-back"
-// to indicate if the operation was successful or not.
-type KafkaResponse struct {
-	// AggregateID is the ID of aggregate the response is for.
+// DocumentMessage can be used to transfer data between services.
+type DocumentMessage struct {
+	// AggregateID is the ID of aggregate the document is for.
 	AggregateID int8 `json:"aggregateID,omitempty"`
 
-	// EventAction is the action corresponding to which the response was produced.
+	// EventAction is the action corresponding to which the document was produced.
 	EventAction string `json:"eventAction,omitempty"`
 
 	// ServiceAction is the service-specific Action for the event.
@@ -17,8 +15,8 @@ type KafkaResponse struct {
 	// informing service that a user was inserted.
 	ServiceAction string `json:"serviceAction,omitempty"`
 
-	// CorrelationID can be used to "identify" responses, such as checking
-	// if the response is for some particular request.
+	// CorrelationID can be used to "link" documents, such as if two documents
+	// are somehow related (for example, when implementing batching).
 	// Including CorrelationID will result in inclusion of this ID in any
 	// responses generated as per result of event's processing.
 	CorrelationID uuuid.UUID `json:"correlationID,omitempty"`
@@ -41,11 +39,11 @@ type KafkaResponse struct {
 	Result []byte `json:"result,omitempty"`
 
 	// Topic is the topic on which Kafka producer should produce this message.
-	// This field will not be included as part of the response, and is only
+	// This field will not be included as part of the document, and is only
 	// for referencing purposes for producer.
 	Topic string `json:"topic,omitempty"`
 
-	// UUID is the V4-UUID Response-Identifier.
+	// UUID is the V4-UUID Document-Identifier.
 	// This can be same as service-query UUID, and can be used for identification puposes.
 	UUID uuuid.UUID `json:"uuid,omitempty"`
 }
